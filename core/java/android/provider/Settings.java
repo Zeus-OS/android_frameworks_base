703d25a2594fe83c693f288886ee7770c6af9074
LorDClockaN <davor@losinj.com>
2015-08-28 20:59:06 +0200

    Toast icon switch [1/2]
    @neobuddy89: Enabled by default
    
    Change-Id: I59ffd10f20a2ae9491f76380ec9af641e0844928
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: ZeNiXxX <zenixxx.havoc@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5975,6 +5975,12 @@ public final class Settings {
         public static final String STATUS_BAR_CLOCK_SECONDS = "status_bar_clock_seconds";
 
         /**
+         * Toast icon
+         * @hide
+         */
+        public static final String TOAST_ICON = "toast_icon";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
