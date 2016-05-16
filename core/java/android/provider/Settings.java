893f84b90c266303cb07fc19fe9be2cdc58422bf
Nico60 <ndhouailly@gmail.com>
2016-05-16 21:48:30 +0200

    Add animations to quick settings tiles [1/2]
    Change-Id: I81e79cfccfc4565b1f7a6292b5f0b88be753df74
    [ jaysonedson: Brought from nougat->android10 ]
    Signed-off-by: Jason Edson <jaysonedson@gmail.com>
    Signed-off-by: ZeNiXxX <zenixxx.havoc@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5049,6 +5049,20 @@ public final class Settings {
         public static final String NAVBAR_STYLE = "navbar_style";
 
         /**
+         * Change quick settings tiles animation style
+         *
+         * @hide
+         */
+        public static final String ANIM_TILE_STYLE = "anim_tile_style";
+
+        /**
+         * Change quick settings tiles animation duration
+         *
+         * @hide
+         */
+        public static final String ANIM_TILE_DURATION = "anim_tile_duration";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
@@ -5184,6 +5198,8 @@ public final class Settings {
             PRIVATE_SETTINGS.add(QS_TILE_STYLE);
             PRIVATE_SETTINGS.add(UI_STYLE);
             PRIVATE_SETTINGS.add(BRIGHTNESS_SLIDER_STYLE);
+            PRIVATE_SETTINGS.add(ANIM_TILE_STYLE);
+            PRIVATE_SETTINGS.add(ANIM_TILE_DURATION);
         }
 
         /**
