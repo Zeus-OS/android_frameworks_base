477f6712ec90cdebe66e2f93039819814b013955
Pranav Vashi <neobuddy89@gmail.com>
2018-01-23 10:55:23 +0530

    CollapsedStatusbar: Fix phantom spaces in status bar
    * Prevent animateShow and animateHide if visibility is set to GONE.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: ZeNiXxX <zenixxx.havoc@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CollapsedStatusBarFragment.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CollapsedStatusBarFragment.java
@@ -334,6 +334,8 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
      * Hides a view.
      */
     private void animateHide(final View v, boolean animate) {
+        if (v.getVisibility() == View.GONE)
+            return;
         animateHiddenState(v, View.INVISIBLE, animate);
     }
 
@@ -341,6 +343,8 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
      * Shows a view, and synchronizes the animation with Keyguard exit animations, if applicable.
      */
     private void animateShow(View v, boolean animate) {
+        if (v.getVisibility() == View.GONE)
+            return;
         v.animate().cancel();
         v.setVisibility(View.VISIBLE);
         if (!animate) {
