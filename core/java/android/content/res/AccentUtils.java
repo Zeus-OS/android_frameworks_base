8d8ee318d31971c0dd4a1217281d86afa8dc618a
Kshitij Gupta <kshitijgm@gmail.com>
2019-04-27 17:33:44 +0530

    base: Introduce RGB accenter [1/2]
    SDB: Adapt for All Normal AOSP Base. Not everyone is comfortable with Dart & Gradle ;)
         Also, adapt for our Gradient Shit
    Signed-off-by: ZeNiXxX <zenixxx.havoc@gmail.com>

---
+++ core/java/android/content/res/AccentUtils.java
@@ -0,0 +1,80 @@
+package android.content.res;
+
+import android.graphics.Color;
+import android.os.SystemProperties;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
+
+/** @hide */
+public class AccentUtils {
+    private static final String TAG = "AccentUtils";
+
+    private static ArrayList<String> accentResources = new ArrayList<>(
+            Arrays.asList("colorAccent",
+                    "accent_device_default",
+                    "accent_device_default_light",
+                    "accent_device_default_dark",
+                    "material_pixel_blue_dark",
+                    "material_pixel_blue_bright",
+                    "holo_blue_light",
+                    "holo_blue_dark",
+                    "omni_color5",
+                    "omni_color4",
+                    "dialer_theme_color",
+                    "dialer_theme_color_dark",
+                    "dialer_theme_color_20pct",
+                    "qs_header_accent_color",
+                    "lockscreen_clock_accent_color",
+                    "recents_dismiss_all_background_color",
+                    "gradient_start"));
+
+    private static ArrayList<String> gradientResources = new ArrayList<>(
+            Arrays.asList("gradient_end"));
+
+    private static final String ACCENT_COLOR_PROP = "persist.sys.theme.accentcolor";
+    private static final String GRADIENT_COLOR_PROP = "persist.sys.theme.gradientcolor";
+
+    static boolean isResourceAccent(String resName) {
+        for (String ar : accentResources)
+            if (resName.contains(ar))
+                return true;
+        return false;
+    }
+
+    static boolean isResourceGradient(String resName) {
+        for (String gr : gradientResources)
+            if (resName.contains(gr))
+                return true;
+        return false;
+    }
+
+    public static int getNewAccentColor(int defaultColor) {
+        return getAccentColor(defaultColor, ACCENT_COLOR_PROP);
+    }
+
+    public static int getNewGradientColor(int defaultColor) {
+        return getAccentColor(defaultColor, GRADIENT_COLOR_PROP);
+    }
+
+    private static int getAccentColor(int defaultColor, String property) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(new Date());
+        if (cal.get(Calendar.MONTH) == 3 && cal.get(Calendar.DAY_OF_MONTH) == 1) {
+            return ColorUtils.genRandomAccentColor(property == ACCENT_COLOR_PROP);
+        }
+        try {
+            String colorValue = SystemProperties.get(property, "-1");
+            return "-1".equals(colorValue)
+                    ? defaultColor
+                    : Color.parseColor("#" + colorValue);
+        } catch (Exception e) {
+            Log.e(TAG, "Failed to set accent: " + e.getMessage() +
+                    "\nSetting default: " + defaultColor);
+            return defaultColor;
+        }
+    }
+}
