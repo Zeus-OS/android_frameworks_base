5fd2330f1c4210c5bff968be64a0683d5e2b8dd6
Anushek Prasal <anushekprasal@gmail.com>
2019-10-24 20:59:30 +0530

    base: Use ro.build.date.utc to signal upgrades
    Change-Id: Ife453d110f58839bcc1935ecd5bbdb103d5bff89
    Signed-off-by: rohan <purohit.rohan@gmail.com>
    Signed-off-by: ZeNiXxX <zenixxx.havoc@gmail.com>

--- core/java/android/os/Build.java
+++ core/java/android/os/Build.java
@@ -62,11 +62,10 @@ public class Build {
     /** The name of the underlying board, like "goldfish". */
     public static final String BOARD = getString("ro.product.board");
 
-    /**
-     * The build date
+    /** The build date
      * @hide
      */
-    public static final String DATE = getString("ro.build.date");
+    public static final String DATE = getString("ro.build.date.utc");
 
     /**
      * The name of the instruction set (CPU type + ABI convention) of native code.
