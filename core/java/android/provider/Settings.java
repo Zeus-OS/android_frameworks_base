1a89c6d624a1bffca200d1aba360050c108b2f31
Stevespear426 <stephen.k.spear@gmail.com>
2019-10-27 17:14:05 +0100

    AOKP custom system animations (1/2)
    - Work by AOKP team and ported by @cristianomatos @crdroidandroid
    - @neobuddy89: Moved to global.
    
    From: Michael Ifland <msifland@gmail.com>
    Date: Mon, 21 Sep 2015 02:23:37 -0600
    Subject: [PATCH] Animations: Add option 'Random' to System Animations
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    - Work by AOKP team and ported by @cristianomatos @crdroidandroid
    - @neobuddy89: Moved to global.
    
    From: Michael Ifland <msifland@gmail.com>
    Date: Mon, 21 Sep 2015 02:23:37 -0600
    Subject: [PATCH] Animations: Add option 'Random' to System Animations
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    @ZeNiXxX -  Bring to Android 11
    
    Signed-off-by: ZeNiXxX <zenixxx.havoc@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -13495,6 +13495,25 @@ public final class Settings {
         public static final String POWER_BUTTON_VERY_LONG_PRESS =
                 "power_button_very_long_press";
 
+        /**  
+         * AOKP Custom System Animations
+         * @hide
+         */  
+        public static final String[] ACTIVITY_ANIMATION_CONTROLS = new String[] {  
+                "activity_open",
+                "activity_close",
+                "task_open",
+                "task_close",
+                "task_to_front",
+                "task_to_back",
+                "wallpaper_open",
+                "wallpaper_close",
+                "wallpaper_intra_open",
+                "wallpaper_intra_close",
+                "task_open_behind",
+        };  
+        public static final String ANIMATION_CONTROLS_DURATION = "animation_controls_duration";
+
         /**
          * Global settings that shouldn't be persisted.
          *
