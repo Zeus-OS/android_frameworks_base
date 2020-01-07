8026a0090ffc85a9beb00d1b62717624abf27ec7
maxwen <max.weninger@gmail.com>
2020-01-07 19:50:18 +0100

    SystemUI: custom keyguard clock kick off
    Stripped down version of the original,
    remove one line and two line clock.
    
    allow custom clock to be in the same position as the default
    clock. Add bold hour clock back from the days where we
    had custom keyguard clock already
    
    Change-Id: Ic97603a001dfd61e617f94418fc8edd97fa8c274
    Signed-off-by: ZeNiXxX <zenixxx.havoc@gmail.com>

--- packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
+++ packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
@@ -150,7 +150,7 @@ public final class ClockManager {
         LayoutInflater layoutInflater = injectionInflater.injectable(LayoutInflater.from(context));
 
         addBuiltinClock(() -> new DefaultClockController(res, layoutInflater, colorExtractor));
-        addBuiltinClock(() -> new BubbleClockController(res, layoutInflater, colorExtractor));
+        //addBuiltinClock(() -> new BubbleClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new AnalogClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new SpectrumClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new SneekyClockController(res, layoutInflater, colorExtractor));
@@ -337,7 +337,8 @@ public final class ClockManager {
                     .setTitle(plugin::getTitle)
                     .setId(id)
                     .setThumbnail(plugin::getThumbnail)
-                    .setPreview(() -> plugin.getPreview(mWidth, mHeight))
+                     // to make the preview bigger in ThemePicker
+                    .setPreview(() -> plugin.getPreview(mWidth / 2, mHeight / 2))
                     .build());
         }
 
