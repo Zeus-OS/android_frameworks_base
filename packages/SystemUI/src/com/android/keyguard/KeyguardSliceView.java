f03b76d6ae90bb4a872d2a87a24e793f97bb99ec
Ido Ben-Hur <idoybh2@gmail.com>
2020-04-01 05:22:12 +0300

    base: SystemUI: Disable junky LS clock & slice transition animations
    taken inpiration from
    https://github.com/omnirom/android_frameworks_base/commit/7397c914e7da1e5d4b4ca27b4319883096e479e3
    but made w/o a tuner switch, we don't need it
    
    Signed-off-by: ZeNiXxX <zenixxx.havoc@gmail.com>

--- packages/SystemUI/src/com/android/keyguard/KeyguardSliceView.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardSliceView.java
@@ -195,7 +195,7 @@ public class KeyguardSliceView extends LinearLayout implements View.OnClickListe
     @Override
     public void onVisibilityAggregated(boolean isVisible) {
         super.onVisibilityAggregated(isVisible);
-        setLayoutTransition(isVisible ? mLayoutTransition : null);
+        setLayoutTransition(null);
     }
 
     private int getLockDateSize() {
@@ -579,7 +579,7 @@ public class KeyguardSliceView extends LinearLayout implements View.OnClickListe
         @Override
         public void onVisibilityAggregated(boolean isVisible) {
             super.onVisibilityAggregated(isVisible);
-            setLayoutTransition(isVisible ? mLayoutTransition : null);
+            setLayoutTransition(null);
         }
 
         @Override
