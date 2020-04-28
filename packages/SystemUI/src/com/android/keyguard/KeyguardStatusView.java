08fc811c2ef9662e70303536638040ce578a78ee
SuperDroidBond <superdroidbond@yahoo.com>
2020-04-28 12:27:20 +0200

    Lockscreen Clock Widget Fonts [1/2]
    SuperDroidBond-:
    Forward Port to Q.
    Add all new fonts for LS.
    Move Font from KeyguardStatusView to KeyguardClockSwitch
    Now we got 30+ options to choose from
    
    This is @beanstown106's idea, we got talking while messing around with some layouts
    and decided that this should be a mod and so here it is :-)
    
    Currently there are 12 fonts to include default font, will add more as time goes by
    
    http://i.imgur.com/adKTfsM.png
    
    http://i.imgur.com/XyaACLw.jpg
    
    BenzoEdit:
    Fixed the fonts being tiny
    
    Signed-off-by: ZeNiXxX <zenixxx.havoc@gmail.com>

--- packages/SystemUI/src/com/android/keyguard/KeyguardStatusView.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardStatusView.java
@@ -21,6 +21,7 @@ import android.app.IActivityManager;
 import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.RemoteException;
@@ -35,7 +36,6 @@ import android.view.View;
 import android.widget.GridLayout;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-
 import androidx.core.graphics.ColorUtils;
 
 import com.android.internal.widget.LockPatternUtils;
@@ -97,6 +97,7 @@ public class KeyguardStatusView extends GridLayout implements
                 refreshTime();
                 updateOwnerInfo();
                 updateLogoutView();
+                mClockView.refreshLockFont();
             }
         }
 
@@ -115,6 +116,7 @@ public class KeyguardStatusView extends GridLayout implements
             refreshFormat();
             updateOwnerInfo();
             updateLogoutView();
+            mClockView.refreshLockFont();
         }
 
         @Override
@@ -191,6 +193,8 @@ public class KeyguardStatusView extends GridLayout implements
         mClockView.setShowCurrentUserTime(true);
         mOwnerInfo = findViewById(R.id.owner_info);
         mKeyguardSlice = findViewById(R.id.keyguard_status_area);
+        mClockView.refreshLockFont();
+
         mTextColor = mClockView.getCurrentTextColor();
 
         mKeyguardSlice.setContentChangeListener(this::onSliceContentChanged);
