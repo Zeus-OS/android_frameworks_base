5e3bcfab90f7e68cea18b258c23b748245982c15
maxwen <max.weninger@gmail.com>
2020-05-02 20:58:05 +0300

    base: Extended suspend actions (1/3)
    Add 3G toggle to the suspend actions mix as
    it is currently one step below the maximum G
    available (4, 4.5).
    
    Change-Id: I961ce8c755261f18c3c482a29c201069083640ba

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5485,6 +5485,11 @@ public final class Settings {
         /**
          * @hide
          */
+        public static final String SCREEN_STATE_THREEG = "screen_state_threeg";
+
+        /**
+         * @hide
+         */
         public static final String SCREEN_STATE_GPS = "screen_state_gps";
 
         /**
