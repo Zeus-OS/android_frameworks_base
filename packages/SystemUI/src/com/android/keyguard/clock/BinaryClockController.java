szhn/frameworks_base

BinaryClock: Use same color behavior as AOSP clocks
- Tint mLockClock according to secondary color

packages/SystemUI/src/com/android/keyguard/clock/BinaryClockController.java
@@ -65,6 +65,11 @@ public class BinaryClockController implements ClockPlugin {
     private ClockLayout mBigClockView;
 
     /**
+     * Helper to extract colors from wallpaper palette for clock face.
+     */
+    private final ClockPalette mPalette = new ClockPalette();
+
+    /**
      * Create a BinaryClockController instance.
      *
      * @param res Resources contains title and thumbnail.
@@ -140,11 +145,21 @@ public class BinaryClockController implements ClockPlugin {
 
     @Override
     public void setTextColor(int color) {
-        mBinaryClock.setTintColor(color);
+        updateColor();
     }
 
     @Override
-    public void setColorPalette(boolean supportsDarkText, int[] colorPalette) {}
+    public void setColorPalette(boolean supportsDarkText, int[] colorPalette) {
+        mPalette.setColorPalette(supportsDarkText, colorPalette);
+        updateColor();
+    }
+
+    private void updateColor() {
+        final int primary = mPalette.getPrimaryColor();
+        final int secondary = mPalette.getSecondaryColor();
+        mBigClockView.setTextColor(secondary);
+        mBinaryClock.setTintColor(primary);
+    }
 
     @Override
     public void onTimeTick() {
@@ -154,6 +169,7 @@ public class BinaryClockController implements ClockPlugin {
 
     @Override
     public void setDarkAmount(float darkAmount) {
+        mPalette.setDarkAmount(darkAmount);
         mBigClockView.setDarkAmount(darkAmount);
         boolean dark = darkAmount == 1;
         mBinaryClock.setDark(dark);

Zeus-OS/android_frameworks_base

Signed-off-by: ZeNiXxX <zenixxx.havoc@gmail.com>

packages/SystemUI/src/com/android/keyguard/clock/BinaryClockController.java
@@ -76,6 +76,11 @@ public class BinaryClockController implements ClockPlugin {
     private TextClock mLockClock;
 
     /**
+     * Helper to extract colors from wallpaper palette for clock face.
+     */
+    private final ClockPalette mPalette = new ClockPalette();
+
+    /**
      * Create a BinaryClockController instance.
      *
      * @param res Resources contains title and thumbnail.
@@ -163,11 +168,21 @@ public class BinaryClockController implements ClockPlugin {
 
     @Override
     public void setTextColor(int color) {
-        mBinaryClock.setTintColor(color);
+        updateColor();
     }
 
     @Override
-    public void setColorPalette(boolean supportsDarkText, int[] colorPalette) {}
+    public void setColorPalette(boolean supportsDarkText, int[] colorPalette) {
+        mPalette.setColorPalette(supportsDarkText, colorPalette);
+        updateColor();
+    }
+
+    private void updateColor() {
+        final int primary = mPalette.getPrimaryColor();
+        final int secondary = mPalette.getSecondaryColor();
+        mLockClock.setTextColor(secondary);
+        mBinaryClock.setTintColor(primary);
+    }
 
     @Override
     public void onTimeTick() {
@@ -178,6 +193,7 @@ public class BinaryClockController implements ClockPlugin {
 
     @Override
     public void setDarkAmount(float darkAmount) {
+        mPalette.setDarkAmount(darkAmount);
         mClockPosition.setDarkAmount(darkAmount);
         mBigClockView.setDarkAmount(darkAmount);
         boolean dark = darkAmount == 1;