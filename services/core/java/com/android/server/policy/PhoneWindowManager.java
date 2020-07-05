d936b328f5145f3118d2cb9fcaef33f19334912f
ZeNiXxX <zenixxx.havoc@gmail.com>
2020-07-05 20:07:10 +0200

    Revert "base: Port extended screenshot function from OOS"
    This reverts commit b146a14843d3f4c8f18939b938cef5ac06980985.

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -210,7 +210,6 @@ import android.view.autofill.AutofillManagerInternal;
 
 import com.android.internal.R;
 import com.android.internal.accessibility.AccessibilityShortcutController;
-import com.android.internal.custom.longshot.ILongScreenshotManager;
 import com.android.internal.logging.MetricsLogger;
 import com.android.internal.logging.nano.MetricsProto;
 import com.android.internal.os.AlternativeDeviceKeyHandler;
@@ -1707,9 +1706,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
         @Override
         public void run() {
-            boolean dockMinimized = mWindowManagerInternal.isMinimizedDock();
             if (!mPocketLockShowing) {
-                mDefaultDisplayPolicy.takeScreenshot(mScreenshotType, dockMinimized);
+                mDefaultDisplayPolicy.takeScreenshot(mScreenshotType);
             }
         }
     }
@@ -1723,7 +1721,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     }
 
     void showGlobalActionsInternal() {
-        stopLongshot();
         if (mGlobalActions == null) {
             mGlobalActions = new GlobalActions(mContext, mWindowManagerFuncs);
         }
@@ -1732,30 +1729,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         // since it took two seconds of long press to bring this up,
         // poke the wake lock so they have some time to see the dialog.
         mPowerManager.userActivity(SystemClock.uptimeMillis(), false);
-    }
-
-    private void stopLongshot() {
-        ILongScreenshotManager shot = ILongScreenshotManager.Stub.asInterface(ServiceManager.getService(Context.LONGSCREENSHOT_SERVICE));
-        if (shot != null) {
-            try {
-                if (shot.isLongshotMode()) {
-                    shot.stopLongshot();
-                }
-            } catch (RemoteException e) {
-                Slog.d(TAG, e.toString());
-            }
-        }
-    }
-
-    @Override
-    public void stopLongshotConnection() {
-        mDefaultDisplayPolicy.stopLongshotConnection();
-    }
-
-    @Override
-    public void takeOPScreenshot(int type) {
-        mScreenshotRunnable.setScreenshotType(type);
-        mHandler.post(mScreenshotRunnable);
     }
 
     boolean isDeviceProvisioned() {
