c508e823fe97a5c0d7b3688e8e6955488d10cb8e
idoybh <idoybh2@gmail.com>
2020-07-06 20:51:40 +0200

    base: Add Linotte font [1/2]

--- packages/overlays/Android.mk
+++ packages/overlays/Android.mk
@@ -66,6 +66,7 @@ LOCAL_REQUIRED_MODULES := \
     FontRoutedGothicRobotoCondensed \
     FontSofiaSans \
     FontSofiaSansSemiCondensed \
+	FontLinotte \
 	IconPackCircularAndroidOverlay \
 	IconPackCircularPixelLauncherOverlay \
 	IconPackCircularPixelThemePickerOverlay \
