3ce440bba4a564b41a6f25d6938ae32a6a708fba
Shubham <skpawar1305@gmail.com>
2020-08-27 16:28:14 +0000

    base: Pulse or wake screen with Hand wave or Pocket gesture [1/2]
    Change-Id: If95b4feada2b470e02b8c4d48312e1f18e4c3c45
    Signed-off-by: ZeNiXxX <zenixxx.havoc@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5446,6 +5446,12 @@ public final class Settings {
         public static final String CUSTOM_AMBIENT_RAISE_GESTURE = "custom_ambient_raise_gesture";
 
         /**
+         * Whether to pulse or wake screen with gestures related to proximity sensors
+         * @hide
+         */
+        public static final String PROXIMITY_SCREEN_WAKE = "proximity_screen_wake";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
