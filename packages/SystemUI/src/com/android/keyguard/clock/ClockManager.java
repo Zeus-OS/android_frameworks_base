d4a4e9342868c2f130d862c4642d41add394c3d7
ZeNiXxX <zenixxx.havoc@gmail.com>
2020-09-02 07:44:58 +0200

    Clock Styles: Add two blue circle animation clocks
    @ZeNiXxX
    
    Signed-off-by: ZeNiXxX <zenixxx.havoc@gmail.com>

--- packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
+++ packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
@@ -157,6 +157,8 @@ public final class ClockManager {
         addBuiltinClock(() -> new SamsungBigHourClockController(res, layoutInflater, colorExtractor, context));
         addBuiltinClock(() -> new BubbleAnimationClockController(res, layoutInflater, colorExtractor, context));
         addBuiltinClock(() -> new RainbowCircleClockController(res, layoutInflater, colorExtractor, context));
+        addBuiltinClock(() -> new BlueCircleClockController(res, layoutInflater, colorExtractor, context));
+        addBuiltinClock(() -> new BlueV2CircleClockController(res, layoutInflater, colorExtractor, context));
         addBuiltinClock(() -> new LoadingClockController(res, layoutInflater, colorExtractor, context));
         addBuiltinClock(() -> new SwingClockController(res, layoutInflater, colorExtractor, context));
         addBuiltinClock(() -> new SamsungClockController(res, layoutInflater, colorExtractor));
