f86eda35c4808408ee4318db4dc5f811d2d6f8ac
Timi Rautamäki <timi.rautamaki@gmail.com>
2020-09-04 22:16:26 +0200

    SystemUI: fix NPE on global actions dialog
    SystemUI would crash when accidentaly double tapping reboot (at least when advanced reboot is enabled)
    
    test: manual
    Change-Id: I0bdb2beda23b77fccedfb70e05f5105f275d56b9
    Signed-off-by: Timi <timi.rautamaki@gmail.com>

--- packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialog.java
+++ packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialog.java
@@ -1240,6 +1240,7 @@ public class GlobalActionsDialog implements DialogInterface.OnDismissListener,
 
         @Override
         public void onClickItem(int position) {
+            if (mDialog == null) return;
             Action item = mAdapter.getItem(position);
             if (!(item instanceof SilentModeTriStateAction)
                 && !(item instanceof AdvancedAction)) {
