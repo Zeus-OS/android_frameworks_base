PixelExtended/frameworks_base

Clock Style : Add lockscreen Rainbow circle animation clock
@ZeNiXxX - Bringup

packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
@@ -171,8 +171,8 @@ public final class ClockManager {
         addBuiltinClock(() -> new TuxClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new BubbleAnimationClockController(res, layoutInflater, colorExtractor, context));
         addBuiltinClock(() -> new RainbowCircleClockController(res, layoutInflater, colorExtractor, context));
-        addBuiltinClock(() -> new FireCircleClockController(res, layoutInflater, colorExtractor, context));
         addBuiltinClock(() -> new LoadingClockController(res, layoutInflater, colorExtractor, context));
+	addBuiltinClock(() -> new FireCircleClockController(res, layoutInflater, colorExtractor, context));
         addBuiltinClock(() -> new SwingClockController(res, layoutInflater, colorExtractor, context));
         // Store the size of the display for generation of clock preview.
         DisplayMetrics dm = res.getDisplayMetrics();

Zeus-OS/android_frameworks_base

packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
@@ -161,6 +161,7 @@ public final class ClockManager {
         addBuiltinClock(() -> new MNMLMinimalClockController(res, layoutInflater, colorExtractor,context));
         addBuiltinClock(() -> new MNMLBoxClockController(res, layoutInflater, colorExtractor, context));
         addBuiltinClock(() -> new BubbleAnimationClockController(res, layoutInflater, colorExtractor, context));
+        addBuiltinClock(() -> new RainbowCircleClockController(res, layoutInflater, colorExtractor, context));
         addBuiltinClock(() -> new LoadingClockController(res, layoutInflater, colorExtractor, context));
         addBuiltinClock(() -> new SwingClockController(res, layoutInflater, colorExtractor, context));
         addBuiltinClock(() -> new TuxClockController(res, layoutInflater, colorExtractor, context));