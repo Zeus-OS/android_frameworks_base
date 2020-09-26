b049e3e99b60cba8ed7e9bee21fa302d5d215c40
Varun Date <date.varun123@gmail.com>
2020-09-26 19:56:10 +0530

    FOD: allow user to disable night light when active [1/2]
    Make this optional for some devices and config for default true on devices which weird light sensors on FOD
    eg: OP7TPRO
    
    Signed-off-by: Varun Date <date.varun123@gmail.com>
    Signed-off-by: ZeNiXxX <zenixxx.havoc@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6506,6 +6506,12 @@ public final class Settings {
          * @hide
          */
         public static final String FOD_COLOR = "fod_color";
+        
+        /**
+         * FOD night light
+         * @hide
+         */
+        public static final String FOD_NIGHT_LIGHT = "fod_night_light";
 
         /**
          * @hide
