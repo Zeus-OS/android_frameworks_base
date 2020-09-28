4003333051c24bb867696169734060771fb1bfb3
Jason Edson <jaysonedson@gmail.com>
2020-09-28 12:25:37 -0700

    EnhancedEstimates: Get estimates from Device Health Services
    Get battery estimates from com.google.android.apps.turbo (Turbo.apk)
    Code taken from android11 SystemUIGoogle.apk and cleaned up
    
    Change-Id: Ic1d1cd4ed2865ffcaed1cab126f699ee4ddf32ab
    Signed-off-by: Jason Edson <jaysonedson@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: ZeNiXxX <zenixxx.havoc@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -14485,6 +14485,12 @@ public final class Settings {
         public static final String HYBRID_SYSUI_BATTERY_WARNING_FLAGS = "hybrid_sysui_battery_warning_flags";
 
         /**
+         * Google Hybrid Battery Warnings
+         * @hide
+         */
+        public static final String HYBRID_SYSUI_BATTERY_WARNING_FLAGS = "hybrid_sysui_battery_warning_flags";
+
+        /**
          * Global settings that shouldn't be persisted.
          *
          * @hide
