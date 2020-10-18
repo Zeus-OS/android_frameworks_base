34c9e48db2812dd119e17471ebfa25f4b8bbffbd
maxwen <max.weninger@gmail.com>
2020-10-18 17:33:26 +0200

    SystemUI: use DOUBLE_TAP_TO_WAKE setting also for wake from aod
    Change-Id: I844e36469b0e3b0168eecb62f6e8b0e9b2bff40e
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Signed-off-by: ZeNiXxX <zenixxx.havoc@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationShadeWindowViewController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationShadeWindowViewController.java
@@ -116,6 +116,9 @@ public class NotificationShadeWindowViewController {
     private boolean mDoubleTapToSleepEnabled;
     private int mQuickQsOffsetHeight;
 
+    // omni additions start
+    private boolean mDoubleTapEnabledNative;
+
     @Inject
     public NotificationShadeWindowViewController(
             InjectionInflationController injectionInflationController,
@@ -184,12 +187,17 @@ public class NotificationShadeWindowViewController {
                 case DOUBLE_TAP_SLEEP_GESTURE:
                     mDoubleTapToSleepEnabled = TunerService.parseIntegerSwitch(newValue, true);
                     break;
+                case Settings.Secure.DOUBLE_TAP_TO_WAKE:
+                    mDoubleTapEnabledNative = Settings.Secure.getIntForUser(mView.getContext().getContentResolver(),
+                            Settings.Secure.DOUBLE_TAP_TO_WAKE, 0, UserHandle.USER_CURRENT) == 1;
+                    break;
             }
         };
         mTunerService.addTunable(tunable,
                 Settings.Secure.DOZE_DOUBLE_TAP_GESTURE,
                 Settings.Secure.DOZE_TAP_SCREEN_GESTURE,
-                DOUBLE_TAP_SLEEP_GESTURE);
+                DOUBLE_TAP_SLEEP_GESTURE,
+                Settings.Secure.DOUBLE_TAP_TO_WAKE);
         mQuickQsOffsetHeight = mView.getResources().getDimensionPixelSize(
                 com.android.internal.R.dimen.quick_qs_offset_height);
 
@@ -212,7 +220,7 @@ public class NotificationShadeWindowViewController {
                             mPowerManager.goToSleep(e.getEventTime());
                             return true;
                         }
-                        if (mDoubleTapEnabled || mSingleTapEnabled) {
+                        if (mDoubleTapEnabled || mSingleTapEnabled || mDoubleTapEnabledNative) {
                             mService.wakeUpIfDozing(
                                     SystemClock.uptimeMillis(), mView, "DOUBLE_TAP");
                             return true;
