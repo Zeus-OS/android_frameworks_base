51e1b4439da76e00148421cf6f4a56437c851375
Danny Lin <danny@kdrag0n.dev>
2021-07-01 11:10:06 +0200

    PixelPropsUtils: Make build property spoofing more reliable
    Determining the package name from Zygote's post-fork process name is
    unreliable for several reasons:
      - If USAP is enabled, Zygote will fork with the USAP process name, and
        spoofing will never trigger when the forked process specializes into
        a specific app.
      - With the native fork loop backported from AOSP master,
        Zygote.setAppProcessName() is no longer used for most apps because
        the forking is handled by native code.
    
    Instrumentation#newApplication() is responsible for creating the
    Application singleton during each app's initialization, so hook it
    instead for a more reliable and robust trigger.
    
    Co-authored-by: althafvly <althafvly@gmail.com>
    Change-Id: Ic762d8853967bf78db614efaf46166e527cb397a
    Signed-off-by: Anush02198 <Anush.4376@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/com/android/internal/os/Zygote.java
+++ core/java/com/android/internal/os/Zygote.java
@@ -36,8 +36,6 @@ import android.system.ErrnoException;
 import android.system.Os;
 import android.util.Log;
 
-import com.android.internal.util.crdroid.PixelPropsUtils;
-
 import dalvik.annotation.optimization.CriticalNative;
 import dalvik.annotation.optimization.FastNative;
 import dalvik.system.ZygoteHooks;
@@ -891,9 +889,6 @@ public final class Zygote {
 
         // Modify model to defy Next-Generation Assistant in the Google app
         maybeSetGoogleModel(args.mPackageName, loggingTag);
-
-        // Set pixel props
-        PixelPropsUtils.setProps(args.mPackageName);
     }
 
     private static final String USAP_ERROR_PREFIX = "Invalid command to USAP: ";
